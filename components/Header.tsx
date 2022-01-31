261dcca33de5b9f6b74b260e633fee67bf5a0e1a
Arun Roberts <arunplaysbass@gmail.com>
2022-02-01 10:00:54 +1100

    added fonts and mixins to sass

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,31 +5,36 @@ import { useContext } from 'react'
 const Header: () => JSX.Element = () => {
 
   return (
-    <nav className='nav'>
-        <ul>
-            <li>
-                <Link href='/'>
-                    <a>00 Home</a>
-                </Link>
-            </li>
-            <li>
-                <Link href='/destination'>
-                    <a>01 Destination</a>
-                </Link>
-            </li>
-            <li>
-                <Link href='/crew'>
-                    <a>02 Crew</a>
-                </Link>
-            </li>
-            <li>
-                <Link href='/technology'>
-                    <a>03 Technology</a>
-                </Link>
-            </li>
+    <>
+        <div>
+            <Image src='/assets/shared/logo.svg' alt='Company logo' width='100' height='100' />
+        </div>
+        <nav className='nav'>
+            <ul>
+                <li>
+                    <Link href='/'>
+                        <a>00 Home</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/destination'>
+                        <a>01 Destination</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/crew'>
+                        <a>02 Crew</a>
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/technology'>
+                        <a>03 Technology</a>
+                    </Link>
+                </li>
 
-        </ul>
-    </nav>
+            </ul>
+        </nav>
+    </>
   )
 }
 
