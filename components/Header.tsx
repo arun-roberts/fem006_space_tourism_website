dbfad8ae448943af443fba9309b521736e77fa03
Arun Roberts <arunplaysbass@gmail.com>
2022-02-02 23:04:50 +1100

    added basic classes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,34 +1,49 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { useContext } from 'react'
+import { useState } from 'react'
 
 const Header: () => JSX.Element = () => {
+    const [isOpen, setIsOpen ] = useState<boolean>(false)
 
   return (
     <>
-        <div>
+        <div className="images">
             <Image src='/assets/shared/logo.svg' alt='Company logo' width='100' height='100' />
+            <Image 
+                onClick={() => setIsOpen(true)} 
+                src='/assets/shared/icon-hamburger.svg' 
+                alt='Click to open navigation'
+                width='5'
+                height='5'
+            />
         </div>
-        <nav className='nav'>
-            <ul>
-                <li>
+        <nav className={`nav ${isOpen ? 'nav--open' : 'nav--closed'}`}>
+            <Image 
+                onClick={() => setIsOpen(false)} 
+                src='/assets/shared/icon-close.svg' 
+                alt='Click to close navigation'
+                width='5'
+                height='5'
+            />
+            <ul className='nav-list'>
+                <li className='nav-list-item'>
                     <Link href='/'>
-                        <a>00 Home</a>
+                        <a className='nav-list-item__text'><span className='nav-list-item__text--number'>00</span> Home</a>
                     </Link>
                 </li>
-                <li>
+                <li className='nav-list-item'>
                     <Link href='/destination'>
-                        <a>01 Destination</a>
+                        <a className='nav-list-item__text'><span className='nav-list-item__text--number'>01</span> Destination</a>
                     </Link>
                 </li>
-                <li>
+                <li className='nav-list-item'>
                     <Link href='/crew'>
-                        <a>02 Crew</a>
+                        <a className='nav-list-item__text'><span className='nav-list-item__text--number'>02</span> Crew</a>
                     </Link>
                 </li>
-                <li>
+                <li className='nav-list-item'>
                     <Link href='/technology'>
-                        <a>03 Technology</a>
+                        <a className='nav-list-item__text'><span className='nav-list-item__text--number'>03</span> Technology</a>
                     </Link>
                 </li>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,15 @@ import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h3>So, you want to travel to</h3>
-      <h1>Space</h1>
-      <p>
+    <div className="landing">
+      <h5 className="landing__heading-five">So, you want to travel to</h5>
+      <h1 className="landing__heading-one">Space</h1>
+      <p className="landing__intro-text">
         Let’s face it; if you want to go to space, you might as well genuinely go to 
         outer space and not hover kind of on the edge of it. Well sit back, and relax 
         because we’ll give you a truly out of this world experience!
       </p>
-      <Link href="/destination"><a>Explore</a></Link>
+      <Link href="/destination"><a className="landing__button">Explore</a></Link>
     </div>
   )
 }
